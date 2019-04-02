llvm-mirror/libcxx

commit 3de658f6b7781ad822eaa62caf05579bc1cdc239

Fix PR#41323 'Race condition in steady_clock::now for _LIBCPP_WIN32API'. thanks to Ivan Afanasyev for the report.
git-svn-id: https://llvm.org/svn/llvm-project/libcxx/trunk@357413 91177308-0d34-0410-b5e6-96231b3b80d8

src/chrono.cpp
@@ -177,16 +177,25 @@ steady_clock::now() _NOEXCEPT
 
 #elif defined(_LIBCPP_WIN32API)
 
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms644905(v=vs.85).aspx says:
+//    If the function fails, the return value is zero. <snip>
+//    On systems that run Windows XP or later, the function will always succeed 
+//      and will thus never return zero.
+
+static LARGE_INTEGER
+__QueryPerformanceFrequency()
+{
+	LARGE_INTEGER val;
+	(void) QueryPerformanceFrequency(&val);
+	return val;
+}
+
 steady_clock::time_point
 steady_clock::now() _NOEXCEPT
 {
-  static LARGE_INTEGER freq;
-  static BOOL initialized = FALSE;
-  if (!initialized)
-    initialized = QueryPerformanceFrequency(&freq); // always succceeds
+  static const LARGE_INTEGER freq = __QueryPerformanceFrequency();
 
-  LARGE_INTEGER counter;
-  QueryPerformanceCounter(&counter);
+  LARGE_INTEGER counter = __QueryPerformanceFrequency();
   return time_point(duration(counter.QuadPart * nano::den / freq.QuadPart));
 }
 

commit 0abd0b7cf7557672971da2b0b6c644c010595c6f

Fix typo that I introduced in r357413. Thanks to ensadc@mailnesia.com for the catch.
git-svn-id: https://llvm.org/svn/llvm-project/libcxx/trunk@357474 91177308-0d34-0410-b5e6-96231b3b80d8

src/chrono.cpp
@@ -195,7 +195,8 @@ steady_clock::now() _NOEXCEPT
 {
   static const LARGE_INTEGER freq = __QueryPerformanceFrequency();
 
-  LARGE_INTEGER counter = __QueryPerformanceFrequency();
+  LARGE_INTEGER counter;
+  (void) QueryPerformanceCounter(&counter);
   return time_point(duration(counter.QuadPart * nano::den / freq.QuadPart));
 }
 